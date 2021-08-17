36d3c8b32153c29c66f3bc1007417d592e74ecc0
Tetsushi Naito <r-top@polaire.org>
2021-08-18 08:11:53 +0900

    first commit

---
+++ templates/submit.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>submit</title>
+</head>
+<body>
+<div id="base">
+    <div id="header">
+        <!-- タイトルバナー -->
+    </div>
+    <div id="submit">
+        <div id="submit_title">
+            <!-- 新規登録画面タイトル -->
+        </div>
+        <div id="input_area">
+            <div class="input_form">
+                <!-- 出発地バス停名の入力 -->
+            </div> 
+            <div class="input_form">
+                <!-- 目的地バス停名の入力 -->
+            </div>
+            <div id="list_num">
+                <!-- 時刻の表示数の選択 --->
+            </div>
+        </div>
+        <div id="submit_button">
+            <!-- submit ボタン --->
+        </div>
+    </div>
+    <div id="footer">
+    <!-- フッター -->
+    </div>
+</div>    
+</body>
+</html>
